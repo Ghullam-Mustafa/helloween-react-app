553b6095ef77feb4d8e1a4936343277fac8abf06
Ghullam Mustafa <gmustafawaince180@gmail.com>
2023-07-11 16:22:33 +0500

    start working

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './css/styles.css'
+import './css/swiper-bundle.min.css'
+import HomePage from './pages/Homepage'
 
-import './App.css';
-
-function App() {
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        
-        
-        
-      </header>
+    <div>
+      <HomePage text="text from app."/>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/pages/Homepage.js
@@ -0,0 +1,14 @@
+import Navbar from '../components/navbar/Navbar'
+import Header from '../components/header/Header'
+
+
+function HomePage(props) {
+    return (
+        <div>
+        <Navbar/>
+        <Header/>
+        </div>
+    )
+}
+
+export default HomePage
